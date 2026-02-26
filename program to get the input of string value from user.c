1d8ec17d08a78502ba64f7fa9b0d50e657d1881a
bishal kanrar M <bishalvishal96@gmail.com>
2026-02-26 13:26:27 +0530

    program to get the input of string value from user.c

---
+++ program to get the input of string value from user.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main () {
+    char src[100]
+    , dest[100];
+    printf("Enter the string value\n");
+    scanf("%s", src);
+    strcpy(dest, src);
+    printf("copied string: %s\n", dest);
+
+}
