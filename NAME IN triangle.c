14fe963e9cfd1ab086294fdc3e37cdfd9a9c0932
bishal kanrar M <bishalvishal96@gmail.com>
2026-02-26 13:27:32 +0530

    NAME IN triangle.c

---
+++ NAME IN triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[] = "bishal";
+    int length = strlen(name);
+
+    for (int i = 1; i <= length; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("%c", name[j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
